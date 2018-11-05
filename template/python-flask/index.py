981c8a3747c375d11aebc151524710e866a7eedf
Brandon Williams <brandon@contiamo.com>
2018-11-05 16:18:35 +0100

    Use real version of of-watchdog

--- template/python-flask/index.py
+++ template/python-flask/index.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
 
-from handler import handle
+import handler
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    data = request.get_data().decode('utf-8')
-    return handle(data)
+@app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
+@app.route("/<path:path>", methods=["POST", "GET"])
+def main_route(path):
+    ret = handler.handle(request.get_data().decode('utf-8'))
+    return ret
 
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
